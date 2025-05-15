6b451e9b12030afdf92639c62a38ebb048a4f8fe
Abhijeet14d <abhijeetsinghwork.01@gmail.com>
2025-05-15 22:02:39 +0530

    update 1.7

--- backend/server.js
+++ backend/server.js
@@ -5,21 +5,30 @@ const cors = require('cors');
 const connectDB = require('./utils/db');
 const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoute');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(
-    cors({
-        origin: '*',
-    })
-);
+const __dirname = path.resolve();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    })
+}
 
 connectDB();
 app.listen(port, ()=>{
