90f065de518063adcc0b83027d38f359a07fd153
Abhijeet14d <abhijeetsinghwork.01@gmail.com>
2025-05-15 22:21:20 +0530

    update 9

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./utils/db');
-const userRoutes = require('./routes/userRoutes');
-const noteRoutes = require('./routes/noteRoute');
+const connectDB = require('./utils/db.js');
+const userRoutes = require('./routes/userRoutes.js');
+const noteRoutes = require('./routes/noteRoute.js');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const __dirname = path.resolve();
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -22,15 +21,14 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
 
-if(process.env.NODE_ENV==="production"){
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
-    })
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
 
 connectDB();
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
